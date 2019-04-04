3b30e84be493435fbcce5a730093fdbc22967b43
Lagas, Alex <alex.lagas@aspentech.com>
2019-04-04 15:20:37 -0400

    First tests setup

--- Steps/Connect.py
+++ Steps/Connect.py
@@ -3,16 +3,21 @@ import requests
 
 global resp
 
+
 @scenario('Connect.feature','Client requests a list of Aspen Connect instances (GET /get_devices)')
 def test_connect():
     pass
 
+
 @given("The client is connected to a valid server instance")
 def valid_connect():
+    assert True
+
 
 @when("The client requests a list of instances")
 def request_instances():
     resp = requests.get('http://10.16.51.178:6584/get_devices')
+
 
 @then("The client should receive a list of instances")
 def check_instances():
