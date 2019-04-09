a7058a7c4e9aa0397047440b3783ae8c8adc388f
Lagas, Alex <alex.lagas@aspentech.com>
2019-04-09 15:13:10 -0400

    Cleaned code

--- Steps/Connect.feature
+++ Steps/Connect.feature
@@ -1,110 +1,92 @@
 Feature: Connect APIs
+  All the APIs for the Connect system
 
-  Scenario Outline: Client requests a list of Aspen Connect instances (GET /get_devices)
+  Scenario: Client requests a list of Aspen Connect instances (GET /get_devices)
     Given: The client is connected to a valid server instance
     When: The client requests a list of instances
     Then: The client should receive a list of instances
-    Examples:
-      |  |
-  Scenario Outline: Client requests information for currently connected device (GET /get_device_info)
+
+  Scenario: Client requests information for currently connected device (GET /get_device_info)
     Given: The client is connected to a valid server instance
     When: The client requests connected device data
     Then: The client should receive connected device data
-    Examples:
-      |  |
-  Scenario Outline: Client requests device stats for currently connected device (GET /get_device_stats)
+
+  Scenario: Client requests device stats for currently connected device (GET /get_device_stats)
     Given: The client is connected to a valid server instance
     When: The client requests connected device statistics
     Then: The client should receive connected device statistics
-    Examples:
-      |  |
-  Scenario Outline: Client requests a list of JSon schemas that define protocol plugins for currently connected device (GET /get_schemas)
+
+  Scenario: Client requests a list of JSon schemas that define protocol plugins for currently connected device (GET /get_schemas)
     Given: The client is connected to a valid server instance
     When: The client requests connected device schemas
     Then: The client should receive connected device schemas
-    Examples:
-      |  |
-  Scenario Outline: Client creates an instance of a protocol plugin as a standalone server (POST /add_server)
+
+  Scenario: Client creates an instance of a protocol plugin as a standalone server (POST /add_server)
     Given: The client is connected to a valid server instance
     When: The client adds a server instance
     Then: A new server instance should be added
-    Examples:
-      |  |
-  Scenario Outline: Client requests a list of all configured, connected servers (GET /get_servers)
+
+  Scenario: Client requests a list of all configured, connected servers (GET /get_servers)
     Given: The client is connected to a valid server instance
     When: The client requests a list of servers
     Then: The client should receive a list of servers
-    Examples:
-      |  |
-  Scenario Outline: Client deletes a given server (POST /delete_server)
+
+  Scenario: Client deletes a given server (POST /delete_server)
     Given: The client is connected to a valid server instance
     When: The client deletes a given server
     Then: The server should be deleted
-    Examples:
-      |  |
-  Scenario Outline: Client requests a list of devices (POST /test_connection)
+
+  Scenario: Client requests a list of devices (POST /test_connection)
     Given: The client is connected to a valid server instance
     When: The client request a list of devices
     Then: The client should receive a list of devices
-    Examples:
-      |  |
-  Scenario Outline: Client requests a list discovered tags on connected server instance (POST /get_available_tags)
+
+  Scenario: Client requests a list discovered tags on connected server instance (POST /get_available_tags)
     Given: The client is connected to a valid server instance
     When: The client requests a list of tags
     Then: The client should receive a list of tags
-    Examples:
-      |  |
-  Scenario Outline: Client saves selected tags onto server instance's local storage (POST /activate_tags)
+
+  Scenario: Client saves selected tags onto server instance's local storage (POST /activate_tags)
     Given: The client is connected to a valid server instance
     When: The client saves a set of tags
     Then: The set of tags should be saved
-    Examples:
-      |  |
-  Scenario Outline: Client saves selected tags onto server instance's local storage (POST /create_tags)
+
+  Scenario: Client saves selected tags onto server instance's local storage (POST /create_tags)
     Given: The client is connected to a valid server instance
     When: The client creates a set of tags
     Then: The tags should be created
-    Examples:
-      |  |
-  Scenario Outline: Client deletes tags with corresponding IDs (POST /delete_tags)
+
+  Scenario: Client deletes tags with corresponding IDs (POST /delete_tags)
     Given: The client is connected to a valid server instance
     When: The client deletes a set of tags
     Then: The set of tags should be deleted
-    Examples:
-      |  |
-  Scenario Outline: Client creates route to define input/output server/tags (POST /create_route)
+
+  Scenario: Client creates route to define input/output server/tags (POST /create_route)
     Given: The client is connected to a valid server instance
     When: The client creates a route
     Then: The route should be created
-    Examples:
-      |  |
-  Scenario Outline: Client requests a list of routes related to the given server id (POST /get_routes)
+
+  Scenario: Client requests a list of routes related to the given server id (POST /get_routes)
     Given: The client is connected to a valid server instance
     When: The client requests a list of routes
     Then: The client should receive a list of routes
-    Examples:
-      |  |
-  Scenario Outline: Client deletes route with a given ID (POST /delete_route)
+
+  Scenario: Client deletes route with a given ID (POST /delete_route)
     Given: The client is connected to a valid server instance
     When: The client deletes a given route
     Then: The route should be deleted
-    Examples:
-      |  |
-  Scenario Outline: Client creates a set of derived tags (POST /create_derived_tags)
+
+  Scenario: Client creates a set of derived tags (POST /create_derived_tags)
     Given: The client is connected to a valid server instance
     When: The client creates a set of derived tags
     Then: The set of derived tags should be created
-    Examples:
-      |  |
-  Scenario Outline: Client deletes a set of derived tags (POST /delete_derived_tags)
+
+  Scenario: Client deletes a set of derived tags (POST /delete_derived_tags)
     Given: The client is connected to a valid server instance
     When: The client deletes a set of derived tags
     Then: The set of derived tags should be deleted
-    Examples:
-      |  |
-  Scenario Outline: Client activates a device with license data (POST /activate_device)
+
+  Scenario: Client activates a device with license data (POST /activate_device)
     Given: The client is connected to a valid server instance
     When: The client activates a device
     Then: The device should be activated
-    Examples:
-      |  |
--- Steps/Connect.py
+++ Steps/Connect.py
@@ -1,22 +1,25 @@
+import pytest
 from pytest_bdd import scenario, given, when, then
 import requests
+import json
 
 global resp
+URL = "10.16.51.178:6584"
 
 
 @scenario('Connect.feature', 'Client requests a list of Aspen Connect instances (GET /get_devices)')
-def test_connect():
-    pass
+def scenario_get_devices():
+    assert True
 
 
 @given("The client is connected to a valid server instance")
-def valid_connect():
+def valid_connect_get_devices():
     assert True
 
 
 @when("The client requests a list of instances")
 def request_instances():
-    resp = requests.get('http://10.16.51.178:6584/get_devices')
+    resp = requests.get('http://' + URL + '/get_devices')
 
 
 @then("The client should receive a list of instances")
@@ -25,18 +28,18 @@ def check_instances():
 
 
 @scenario('Connect.feature', 'Client requests information for currently connected device (GET /get_device_info)')
-def test_connect():
-    pass
+def scenario_get_device_info():
+    assert True
 
 
 @given("The client is connected to a valid server instance")
-def valid_connect():
+def valid_connect_get_device_info():
     assert True
 
 
 @when("The client requests connected device data")
 def request_device_info():
-    resp = requests.get('http://10.16.51.178:6584/get_device_info')
+    resp = requests.get('http://' + URL + '/get_device_info')
 
 
 @then("The client should receive connected device data")
@@ -45,18 +48,18 @@ def check_device_info():
 
 
 @scenario('Connect.feature', 'Client requests information for currently connected device (GET /get_device_stats)')
-def test_connect():
-    pass
+def scenario_get_device_stats():
+    assert True
 
 
 @given("The client is connected to a valid server instance")
-def valid_connect():
+def valid_connect_get_device_stats():
     assert True
 
 
 @when("The client requests connected device statistics")
 def request_device_stats():
-    resp = requests.get('http://10.16.51.178:6584/get_device_stats')
+    resp = requests.get('http://' + URL + '/get_device_stats')
 
 
 @then("The client should receive connected device statistics")
@@ -65,300 +68,300 @@ def check_device_stats():
 
 
 @scenario('Connect.feature', 'Client requests a list of JSon schemas that define protocol plugins for currently connected device (GET /get_schemas)')
-def test_connect():
-    pass
+def scenario_get_schemas():
+    assert True
 
 
 @given("The client is connected to a valid server instance")
-def valid_connect():
+def valid_connect_get_schemas():
     assert True
 
 
 @when("The client requests connected device schemas")
-def request_device_stats():
-    resp = requests.get('http://10.16.51.178:6584/get_schemas')
+def request_device_schemas():
+    resp = requests.get('http://' + URL + '/get_schemas')
 
 
 @then("The client should receive connected device schemas")
-def check_device_stats():
+def check_device_schemas():
     assert (resp.status_code == 200)
 
 
 @scenario('Connect.feature', 'Client creates an instance of a protocol plugin as a standalone server (POST /add_server)')
-def test_connect():
-    pass
+def scenario_add_server():
+    assert True
 
 
 @given("The client is connected to a valid server instance")
-def valid_connect():
+def valid_connect_add_server():
     assert True
 
 
 @when("The client adds a server instance")
-def request_device_stats():
-    resp = requests.post('http://10.16.51.178:6584/add_server')
+def add_server():
+    resp = requests.post('http://' + URL + '/add_server')
 
 
 @then("A new server instance should be added")
-def check_device_stats():
+def check_server():
     assert (resp.status_code == 200)
 
 
 @scenario('Connect.feature', 'Client requests a list of all configured, connected servers (GET /get_servers)')
-def test_connect():
-    pass
+def scenario_get_servers():
+    assert True
 
 
 @given("The client is connected to a valid server instance")
-def valid_connect():
+def valid_connect_get_servers():
     assert True
 
 
 @when("The client requests a list of servers")
-def request_device_stats():
-    resp = requests.get('http://10.16.51.178:6584/get_servers')
+def get_servers():
+    resp = requests.get('http://' + URL + '/get_servers')
 
 
 @then("The client should receive a list of servers")
-def check_device_stats():
+def check_servers():
     assert (resp.status_code == 200)
 
 
 @scenario('Connect.feature', 'Client deletes a given server (POST /delete_server)')
-def test_connect():
-    pass
+def scenario_delete_server():
+    assert True
 
 
 @given("The client is connected to a valid server instance")
-def valid_connect():
+def valid_connect_delete_server():
     assert True
 
 
 @when("The client deletes a given server")
-def request_device_stats():
-    resp = requests.post('http://10.16.51.178:6584/delete_server')
+def delete_server():
+    resp = requests.post('http://' + URL + '/delete_server')
 
 
 @then("The server should be deleted")
-def check_device_stats():
+def check_delete_server():
     assert (resp.status_code == 200)
 
 
 @scenario('Connect.feature', 'Client requests a list of devices (POST /test_connection)')
-def test_connect():
-    pass
+def scenario_test_connection():
+    assert True
 
 
 @given("The client is connected to a valid server instance")
-def valid_connect():
+def valid_connect_test_connection():
     assert True
 
 
 @when("The client request a list of devices")
-def request_device_stats():
-    resp = requests.post('http://10.16.51.178:6584/test_connection')
+def test_connection():
+    resp = requests.post('http://' + URL + '/test_connection')
 
 
 @then("The client should receive a list of devices")
-def check_device_stats():
+def check_test():
     assert (resp.status_code == 200)
 
 
 @scenario('Connect.feature', 'Client requests a list discovered tags on connected server instance (POST /get_available_tags)')
-def test_connect():
-    pass
+def scenario_get_available_tags():
+    assert True
 
 
 @given("The client is connected to a valid server instance")
-def valid_connect():
+def valid_connect_get_available_tags():
     assert True
 
 
 @when("The client requests a list of tags")
-def request_device_stats():
-    resp = requests.post('http://10.16.51.178:6584/get_available_tags')
+def get_available_tags():
+    resp = requests.post('http://' + URL + '/get_available_tags')
 
 
 @then("The client should receive a list of tags")
-def check_device_stats():
+def check_available_tags():
     assert (resp.status_code == 200)
 
 
 @scenario('Connect.feature', "Client saves selected tags onto server instance's local storage (POST /activate_tags)")
-def test_connect():
-    pass
+def scenario_activate_tags():
+    assert True
 
 
 @given("The client is connected to a valid server instance")
-def valid_connect():
+def valid_connect_activate_tags():
     assert True
 
 
 @when("The client saves a set of tags")
-def request_device_stats():
-    resp = requests.post('http://10.16.51.178:6584/activate_tags')
+def activate_tags():
+    resp = requests.post('http://' + URL + '/activate_tags')
 
 
 @then("The set of tags should be saved")
-def check_device_stats():
+def check_activated_tags():
     assert (resp.status_code == 200)
 
 
 @scenario('Connect.feature', "Client saves selected tags onto server instance's local storage (POST /create_tags)")
-def test_connect():
-    pass
+def scenario_create_tags():
+    assert True
 
 
 @given("The client is connected to a valid server instance")
-def valid_connect():
+def valid_connect_create_tags():
     assert True
 
 
 @when("The client creates a set of tags")
-def request_device_stats():
-    resp = requests.post('http://10.16.51.178:6584/create_tags')
+def create_tags():
+    resp = requests.post('http://' + URL + '/create_tags')
 
 
 @then("The tags should be created")
-def check_device_stats():
+def check_created_tags():
     assert (resp.status_code == 200)
 
 
 @scenario('Connect.feature', "Client deletes tags with corresponding IDs (POST /delete_tags)")
-def test_connect():
-    pass
+def scenario_delete_tags():
+    assert True
 
 
 @given("The client is connected to a valid server instance")
-def valid_connect():
+def valid_connect_delete_tags():
     assert True
 
 
 @when("The client deletes a set of tags")
-def request_device_stats():
-    resp = requests.post('http://10.16.51.178:6584/delete_tags')
+def delete_tags():
+    resp = requests.post('http://' + URL + '/delete_tags')
 
 
 @then("The set of tags should be deleted")
-def check_device_stats():
+def check_deleted_tags():
     assert (resp.status_code == 200)
 
 
 @scenario('Connect.feature', "Client creates route to define input/output server/tags (POST /create_route)")
-def test_connect():
-    pass
+def scenario_create_route():
+    assert True
 
 
 @given("The client is connected to a valid server instance")
-def valid_connect():
+def valid_connect_create_route():
     assert True
 
 
 @when("The client creates a route")
-def request_device_stats():
-    resp = requests.post('http://10.16.51.178:6584/create_route')
+def create_route():
+    resp = requests.post('http://' + URL + '/create_route')
 
 
 @then("The route should be created")
-def check_device_stats():
+def check_route():
     assert (resp.status_code == 200)
 
 
 @scenario('Connect.feature', "Client requests a list of routes related to the given server id (POST /get_routes)")
-def test_connect():
-    pass
-
-
-@given("The client is connected to a valid server instance")
-def valid_connect():
+def scenario_get_routes():
     assert True
 
 
-@when("The client requests a list of routes")
-def request_device_stats():
-    resp = requests.post('http://10.16.51.178:6584/get_routes')
+@given("The client is connected to a valid server instance")
+def valid_connect_get_routes():
+    assert True
 
 
-@then("The client should receive a list of routes")
-def check_device_stats():
+@when('The client requests a list of routes')
+def get_routes():
+    resp = requests.post('http://' + URL + '/get_routes')
+
+
+@then('The client should receive a list of routes')
+def check_routes():
     assert (resp.status_code == 200)
 
 
-@scenario('Connect.feature', "Client deletes route with a given ID (POST /delete_route)")
-def test_connect():
-    pass
-
-
-@given("The client is connected to a valid server instance")
-def valid_connect():
+@scenario('Connect.feature', 'Client deletes route with a given ID (POST /delete_route)')
+def scenario_delete_route():
     assert True
 
 
-@when("The client deletes a given route")
-def request_device_stats():
-    resp = requests.post('http://10.16.51.178:6584/delete_route')
+@given('The client is connected to a valid server instance')
+def valid_connect_delete_route():
+    assert True
 
 
-@then("The route should be deleted")
-def check_device_stats():
+@when('The client deletes a given route')
+def delete_route():
+    resp = requests.post('http://' + URL + '/delete_route')
+
+
+@then('The route should be deleted')
+def check_deleted_route():
     assert (resp.status_code == 200)
 
 
-@scenario('Connect.feature', "Client creates a set of derived tags (POST /create_derived_tags)")
-def test_connect():
-    pass
-
-
-@given("The client is connected to a valid server instance")
-def valid_connect():
+@scenario('Connect.feature', 'Client creates a set of derived tags (POST /create_derived_tags)')
+def scenario_create_derived_tags():
     assert True
 
 
-@when("The client creates a set of derived tags")
-def request_device_stats():
-    resp = requests.post('http://10.16.51.178:6584/create_derived_tags')
+@given('The client is connected to a valid server instance')
+def valid_connect_create_derived_tags():
+    assert True
 
 
-@then("The set of derived tags should be created")
-def check_device_stats():
+@when('The client creates a set of derived tags')
+def create_derived_tags():
+    resp = requests.post('http://' + URL + '/create_derived_tags')
+
+
+@then('The set of derived tags should be created')
+def check_derived_tags():
     assert (resp.status_code == 200)
 
 
 @scenario('Connect.feature', "Client deletes a set of derived tags (POST /delete_derived_tags)")
-def test_connect():
-    pass
+def scenario_delete_derived_tags():
+    assert True
 
 
 @given("The client is connected to a valid server instance")
-def valid_connect():
+def valid_connect_delete_derived_tags():
     assert True
 
 
 @when("The client deletes a set of derived tags")
-def request_device_stats():
-    resp = requests.post('http://10.16.51.178:6584/delete_derived_tags')
+def delete_derived_tags():
+    resp = requests.post('http://' + URL + '/delete_derived_tags')
 
 
 @then("The set of derived tags should be deleted")
-def check_device_stats():
+def check_deleted_derived_tags():
     assert (resp.status_code == 200)
 
 
 @scenario('Connect.feature', "Client activates a device with license data (POST /activate_device)")
-def test_connect():
-    pass
+def scenario_activate_device():
+    assert True
 
 
 @given("The client is connected to a valid server instance")
-def valid_connect():
+def valid_connect_activate_device():
     assert True
 
 
 @when("The client activates a device")
-def request_device_stats():
-    resp = requests.post('http://10.16.51.178:6584/activate_device')
+def activated_device():
+    resp = requests.post('http://' + URL + '/activate_device')
 
 
 @then("The device should be activated")
-def check_device_stats():
+def check_activation():
     assert (resp.status_code == 200)
